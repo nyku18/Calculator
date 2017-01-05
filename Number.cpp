a737cdad47d3cd2d8d7d3de52e94f363f6608fb0
nyku18 <nicolae.raulea18@gmail.com>
2017-01-05 18:04:07 +0200

    Add vcl header in Number class

---
+++ Number.cpp
@@ -0,0 +1,63 @@
+//---------------------------------------------------------------------------
+
+
+#pragma hdrstop
+
+#include "Number.h"
+//#include <string.h>
+#include <vcl.h>
+
+//---------------------------------------------------------------------------
+
+#pragma package(smart_init)
+
+Number::Number()
+{
+
+}
+
+char* Number::StringToChar(String SNumberDigits)
+{
+        char CNumberDigits[100] = "";
+        strcpy(CNumberDigits, SNumberDigits.c_str());
+
+        return CNumberDigits;
+}
+
+int* Number::CharToInt(char* CNumberDigits)
+{
+        int i;
+
+        for(i = 0; i < NumberDigitsLength; i++)
+        {
+                NumberDigits[i] = CNumberDigits[i] - '0';
+        }
+
+        return NumberDigits;
+}
+
+void Number::StringToInt(String SNumberDigits)
+{
+        NumberDigitsLength = strlen(SNumberDigits.c_str());
+
+        char CNumberDigits[100];
+        memcpy(CNumberDigits, StringToChar(SNumberDigits), sizeof(CNumberDigits));
+
+        memcpy(NumberDigits, CharToInt(CNumberDigits, NumberDigitsLength), sizeof(NumberDigits));
+
+        //return NumberDigits;
+}
+
+int* Number::Overturn(int* NumberDigits)
+{
+        int i;
+
+        int OverturnedNumberDigits[100];
+
+        for(i = 0; i < NumberDigitsLength; i++)
+        {
+                OverturnedNumberDigits[NumberDigitsLength - i - 1] = NumberDigits[i];
+        }
+
+        return OverturnedNumberDigits;
+}
---
+++ Number.~h
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#ifndef NumberH
+#define NumberH
+//---------------------------------------------------------------------------
+#include <string>
+#include <vcl.h>
+//---------------------------------------------------------------------------
+class Number
+{
+        public:
+
+                int NumberDigits[100];
+                int NumberDigitsLength;
+                Number();
+                void StringToInt(String SNumberDigits);
+                int* Overturn(int* NumberDigits);
+        private:
+                char* StringToChar(String SNumberDigits);
+                int* CharToInt(char* CNumberDigits);
+};
+
+
+//---------------------------------------------------------------------------
+#endif
